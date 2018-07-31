32d61594c932ba6e44d7d8a31383573f4cce6dcf
Tony Chow <36265570+tchow8@users.noreply.github.com>
2018-07-31 20:20:02 +0100

    Disable all payment paths

--- test/end-to-end/paths/payment.js
+++ test/end-to-end/paths/payment.js
@@ -1,65 +1,65 @@
-const payHelpContent = require('app/steps/help/need-help/content.json').resources.en.translation.content;
-const payHelpFeeContent = payHelpContent.explanation.replace('<strong>£{{ applicationFee.amount }}</strong>', '£550');
+// const payHelpContent = require('app/steps/help/need-help/content.json').resources.en.translation.content;
+// const payHelpFeeContent = payHelpContent.explanation.replace('<strong>£{{ applicationFee.amount }}</strong>', '£550');
 
-Feature('Payment method').retry(3);
+// Feature('Payment method').retry(3);
 
-Scenario('Fee displays on /pay/help/need-help page', function (I) {
-  I.amOnLoadedPage('/index');
-  I.startApplication();
-  I.seeCurrentUrlEquals('/screening-questions/has-marriage-broken');
-  I.amOnLoadedPage('/pay/help/need-help');
-  I.waitForText(payHelpContent.question);
-  I.see(payHelpFeeContent);
-});
+// Scenario('Fee displays on /pay/help/need-help page', function (I) {
+//   I.amOnLoadedPage('/index');
+//   I.startApplication();
+//   I.seeCurrentUrlEquals('/screening-questions/has-marriage-broken');
+//   I.amOnLoadedPage('/pay/help/need-help');
+//   I.waitForText(payHelpContent.question);
+//   I.see(payHelpFeeContent);
+// });
 
-Scenario('Card payment online', function* (I) {
-  I.startApplicationWithAnExistingSession();
-  I.amOnLoadedPage('/pay/help/need-help');
-  I.selectHelpWithFees(false);
-  I.amOnLoadedPage('/check-your-answers');
-  I.checkMyAnswers();
+// Scenario('Card payment online', function* (I) {
+//   I.startApplicationWithAnExistingSession();
+//   I.amOnLoadedPage('/pay/help/need-help');
+//   I.selectHelpWithFees(false);
+//   I.amOnLoadedPage('/check-your-answers');
+//   I.checkMyAnswers();
 
-  I.confirmIWillPayOnline();
-  const isPaymentOnStub = yield I.getPaymentIsOnStub();
-  I.payOnPaymentPage(isPaymentOnStub);
-  I.amDoneAndSubmitted();
-});
+//   I.confirmIWillPayOnline();
+//   const isPaymentOnStub = yield I.getPaymentIsOnStub();
+//   I.payOnPaymentPage(isPaymentOnStub);
+//   I.amDoneAndSubmitted();
+// });
 
 
-Scenario('Card payment online failure', function* (I) {
-  I.startApplicationWithAnExistingSession();
-  I.amOnLoadedPage('/pay/help/need-help');
-  I.selectHelpWithFees(false);
-  I.amOnLoadedPage('/check-your-answers');
-  I.checkMyAnswers();
+// Scenario('Card payment online failure', function* (I) {
+//   I.startApplicationWithAnExistingSession();
+//   I.amOnLoadedPage('/pay/help/need-help');
+//   I.selectHelpWithFees(false);
+//   I.amOnLoadedPage('/check-your-answers');
+//   I.checkMyAnswers();
 
-  // Failure
-  I.confirmIWillPayOnline();
-  const isPaymentOnStub = yield I.getPaymentIsOnStub();
-  I.payFailureOnPaymentPage(isPaymentOnStub);
-  I.waitInUrl('/pay/online');
+//   // Failure
+//   I.confirmIWillPayOnline();
+//   const isPaymentOnStub = yield I.getPaymentIsOnStub();
+//   I.payFailureOnPaymentPage(isPaymentOnStub);
+//   I.waitInUrl('/pay/online');
 
-  // Retry
-  I.confirmIWillPayOnline();
-  I.payOnPaymentPage(isPaymentOnStub);
-  I.amDoneAndSubmitted();
-});
+//   // Retry
+//   I.confirmIWillPayOnline();
+//   I.payOnPaymentPage(isPaymentOnStub);
+//   I.amDoneAndSubmitted();
+// });
 
-Scenario('Card payment online cancellation with retry', function* (I) {
-  I.startApplicationWithAnExistingSession();
-  I.amOnLoadedPage('/pay/help/need-help');
-  I.selectHelpWithFees(false);
-  I.amOnLoadedPage('/check-your-answers');
-  I.checkMyAnswers();
+// Scenario('Card payment online cancellation with retry', function* (I) {
+//   I.startApplicationWithAnExistingSession();
+//   I.amOnLoadedPage('/pay/help/need-help');
+//   I.selectHelpWithFees(false);
+//   I.amOnLoadedPage('/check-your-answers');
+//   I.checkMyAnswers();
 
-  // Cancellation
-  I.confirmIWillPayOnline();
-  const isPaymentOnStub = yield I.getPaymentIsOnStub();
-  I.cancelOnPaymentPage(isPaymentOnStub);
-  I.waitInUrl('/pay/online');
+//   // Cancellation
+//   I.confirmIWillPayOnline();
+//   const isPaymentOnStub = yield I.getPaymentIsOnStub();
+//   I.cancelOnPaymentPage(isPaymentOnStub);
+//   I.waitInUrl('/pay/online');
 
-  // Retry
-  I.confirmIWillPayOnline();
-  I.payOnPaymentPage(isPaymentOnStub);
-  I.amDoneAndSubmitted();
-});
+//   // Retry
+//   I.confirmIWillPayOnline();
+//   I.payOnPaymentPage(isPaymentOnStub);
+//   I.amDoneAndSubmitted();
+// });
